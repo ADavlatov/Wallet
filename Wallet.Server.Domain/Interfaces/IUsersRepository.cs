382c610c89425ea98db46785626128e8fc5094a4
ADavlatov <a@davlatov.space>
2025-02-20 23:43:06 +0300

    Fix

--- Wallet.Server.Domain/Interfaces/IUsersRepository.cs
+++ Wallet.Server.Domain/Interfaces/IUsersRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Wallet.Server.Domain.Interfaces;
+﻿using Wallet.Server.Domain.Entities;
+
+namespace Wallet.Server.Domain.Interfaces;
 
 public interface IUsersRepository
 {
-    
+    Task<IResult> AddUser(User user, CancellationToken cancellationToken);
+    Task<IResult> GetAllUsers(CancellationToken cancellationToken);
+    Task<IResult> GetUserById(Guid id, CancellationToken cancellationToken);
+    Task<IResult> GetUserByUsername(string username, CancellationToken cancellationToken);
+    Task<IResult> UpdateUser(User user, CancellationToken cancellationToken);
+    Task<IResult> DeleteUser(User user, CancellationToken cancellationToken);
 }
