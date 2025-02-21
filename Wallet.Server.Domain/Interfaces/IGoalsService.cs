07043093396130c4806de8eb938822b472d7534f
ADavlatov <a@davlatov.space>
2025-02-22 01:23:45 +0300

    Add interfaces for services

---
+++ Wallet.Server.Domain/Interfaces/IGoalsService.cs
@@ -0,0 +1,6 @@
+﻿namespace Wallet.Server.Domain.Interfaces;
+
+public class IGoalsService
+{
+    
+}
---
+++ Wallet.Server.Domain/Interfaces/IUsersService.cs
@@ -0,0 +1,8 @@
+﻿using Wallet.Server.Domain.Entities;
+
+namespace Wallet.Server.Domain.Interfaces;
+
+public interface IUsersService
+{
+    
+}
