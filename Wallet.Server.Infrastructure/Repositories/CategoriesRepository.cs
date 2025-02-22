696d80436f5c974f24a5cf159be092d92be8f578
ADavlatov <a@davlatov.space>
2025-02-23 00:16:56 +0300

    Add logic for categories repo

--- Wallet.Server.Infrastructure/Repositories/CategoriesRepository.cs
+++ Wallet.Server.Infrastructure/Repositories/CategoriesRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using FluentResults;
+using Microsoft.EntityFrameworkCore;
 using Wallet.Server.Domain.Entities;
 using Wallet.Server.Domain.Interfaces;
 using Wallet.Server.Infrastructure.Contexts;
@@ -7,33 +8,60 @@ namespace Wallet.Server.Infrastructure.Repositories;
 
 public class CategoriesRepository(WalletContext db) : ICategoriesRepository
 {
-    public async Task<IResult> AddCategory(Category category, CancellationToken cancellationToken)
+    public async Task<Result> AddCategory(Category category, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        await db.Categories.AddAsync(category, cancellationToken);
+        await db.SaveChangesAsync(cancellationToken);
+        
+        return Result.Ok();
     }
 
-    public async Task<IResult> GetAllCategoriesByUserId(Guid userId, CancellationToken cancellationToken)
+    public async Task<Result<List<Category>>> GetAllCategoriesByUserId(Guid userId, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var categories = await db.Categories.Where(x => x.UserId == userId).ToListAsync(cancellationToken);
+        if (!categories.Any())
+        {
+            return Result.Fail("No categories found");
+        }
+
+        return Result.Ok(categories.ToList());
     }
 
-    public async Task<IResult> GetCategoryById(Guid id, CancellationToken cancellationToken)
+    public async Task<Result<Category>> GetCategoryById(Guid id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var category = await db.Categories.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        if (category is null)
+        {
+            return Result.Fail("Category not found");
+        }
+
+        return Result.Ok(category);
     }
 
-    public async Task<IResult> GetCategoryByName(string name, CancellationToken cancellationToken)
+    public async Task<Result<Category>> GetCategoryByName(Guid userId, string name, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var category = await db.Categories.FirstOrDefaultAsync(x => x.UserId == userId && x.Name == name, cancellationToken);
+        if (category is null)
+        {
+            return Result.Fail("Category not found");
+        }
+
+        return Result.Ok(category);
     }
 
-    public async Task<IResult> UpdateCategory(Category category, CancellationToken cancellationToken)
+    public async Task<Result> UpdateCategory(Category updatedCategory, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        db.Categories.Update(updatedCategory);
+        await db.SaveChangesAsync(cancellationToken);
+        
+        return Result.Ok();
     }
 
-    public async Task<IResult> DeleteCategory(Category category, CancellationToken cancellationToken)
+    public async Task<Result> DeleteCategory(Category category, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        db.Categories.Remove(category);
+        await db.SaveChangesAsync(cancellationToken);
+        
+        return Result.Ok();
     }
 }
