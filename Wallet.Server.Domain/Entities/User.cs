81dbf844ff96add10b65d18a434f99ff751b7022
ADavlatov <a@davlatov.space>
2025-02-23 20:36:21 +0300

    Update user entity

--- Wallet.Server.Domain/Entities/User.cs
+++ Wallet.Server.Domain/Entities/User.cs
@@ -1,10 +1,12 @@
 namespace Wallet.Server.Domain.Entities;
 
-public class User(string username, string password)
+public class User(string username, string password, string salt)
 {
     public Guid Id { get; init; }
+    public long? TelegramUserId { get; set; }
     public string Username { get; set; } = username;
     public string Password { get; set; } = password;
+    public string Salt { get; set; } = salt;
     public List<Transaction> Transactions { get; set; } = new();
     public List<Category> Categories { get; set; } = new();
     public List<Goal> Goals { get; set; } = new();
