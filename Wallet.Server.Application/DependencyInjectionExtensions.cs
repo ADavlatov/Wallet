9b78e4d94c01b306897b3eed9051758fb231f6b2
ADavlatov <a@davlatov.space>
2025-02-24 16:25:56 +0300

    Add di extension

---
+++ Wallet.Server.Application/DependencyInjectionExtensions.cs
@@ -0,0 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Wallet.Server.Application;
+
+public static class DependencyInjectionExtensions
+{
+    public static void ConfigureDependencies(this IServiceCollection services)
+    {
+    }
+}
