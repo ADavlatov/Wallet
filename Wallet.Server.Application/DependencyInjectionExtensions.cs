f52fcc0861caba2d9bb3f281e21ade80ab8b12a5
ADavlatov <a@davlatov.space>
2025-02-25 18:49:53 +0300

    Di

--- Wallet.Server.Application/DependencyInjectionExtensions.cs
+++ Wallet.Server.Application/DependencyInjectionExtensions.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using Wallet.Server.Application.Services;
+using Wallet.Server.Domain.Interfaces;
 
 namespace Wallet.Server.Application;
 
@@ -6,5 +8,9 @@ public static class DependencyInjectionExtensions
 {
     public static void ConfigureDependencies(this IServiceCollection services)
     {
+        services.AddTransient<ICategoriesService, CategoriesService>();
+        services.AddTransient<IGoalsService, GoalsService>();
+        services.AddTransient<ITransactionsService, TransactionsService>();
+        services.AddTransient<IUsersService, UsersService>();
     }
 }
--- Wallet.Server.Infrastructure/DependencyInjectionExtensions.cs
+++ Wallet.Server.Infrastructure/DependencyInjectionExtensions.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using Wallet.Server.Domain.Interfaces;
+using Wallet.Server.Infrastructure.Repositories;
 
 namespace Wallet.Server.Infrastructure;
 
@@ -6,6 +8,9 @@ public static class DependencyInjectionExtensions
 {
     public static void ConfigureDependencies(this IServiceCollection services)
     {
-      
+        services.AddTransient<ICategoriesRepository, CategoriesRepository>();
+        services.AddTransient<IGoalsRepository, GoalsRepository>();
+        services.AddTransient<ITransactionsRepository, TransactionsRepository>();
+        services.AddTransient<IUsersRepository, UsersRepository>();
     }
 }
