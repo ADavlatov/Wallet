7cf1471035d361748820c1ff7474a9f1b1436fa8
ADavlatov <a@davlatov.space>
2025-02-26 07:55:22 +0300

    Add di

--- Wallet.Server.Presentation/Program.cs
+++ Wallet.Server.Presentation/Program.cs
@@ -1,5 +1,5 @@
-using System.Reflection;
 using Microsoft.OpenApi.Models;
+using Wallet.Server.Infrastructure.Contexts;
 using Wallet.Server.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,18 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 Wallet.Server.Infrastructure.DependencyInjectionExtensions.ConfigureDependencies(builder.Services);
 Wallet.Server.Application.DependencyInjectionExtensions.ConfigureDependencies(builder.Services);
 
+builder.Services.AddDbContext<WalletContext>();
 builder.Services.AddControllers(x => x.Filters.Add<GlobalExceptionFilter>());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(x =>
 {
     x.SwaggerDoc("v1", new OpenApiInfo { Title = "Wallet", Version = "v1" });
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    x.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    // var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    // x.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
 
 var app = builder.Build();
 
 app.UseRouting();
 app.UseEndpoints(endpoints => endpoints.MapControllers());
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
