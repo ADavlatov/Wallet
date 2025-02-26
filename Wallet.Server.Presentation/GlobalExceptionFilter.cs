a815fa4278142636d0a45c9199e61c35b429af65
ADavlatov <a@davlatov.space>
2025-02-26 16:42:07 +0300

    Update exceptions

--- Wallet.Server.Presentation/GlobalExceptionFilter.cs
+++ Wallet.Server.Presentation/GlobalExceptionFilter.cs
@@ -8,9 +8,20 @@ public class GlobalExceptionFilter : IExceptionFilter
 {
     public void OnException(ExceptionContext context)
     {
+        Console.WriteLine(context.Exception);
         if (context.Exception is NotFoundException)
         {
             context.Result = new NotFoundResult();
+            context.ExceptionHandled = true;
+        }
+        if (context.Exception is AlreadyExistsException)
+        {
+            context.Result = new ContentResult();
+            context.ExceptionHandled = true;
+        }
+        if (context.Exception is AuthenticationException)
+        {
+            context.Result = new UnauthorizedResult();
             context.ExceptionHandled = true;
         }
         else
